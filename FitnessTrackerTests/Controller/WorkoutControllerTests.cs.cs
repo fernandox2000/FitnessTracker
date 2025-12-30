fa567f05ff20dc1184796e51be5f49e2c9745b39
Fernando Bertholino <fernandoxab@hotmail.com>
2025-12-30 12:51:44 -0300

    criando projeto de teste

---
+++ FitnessTrackerTests/Controller/WorkoutControllerTests.cs.cs
@@ -0,0 +1,16 @@
+namespace FitnessTrackerTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
