2bdbe6d19b106d1ab05f4f9f558e1e7e63f72f81
Fernando Bertholino <fernandoxab@hotmail.com>
2026-01-22 17:34:15 -0300

    adding first test using inMemory persistence

---
+++ FitnessTrackerTests/Services/WorkoutServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using FitnessTracker.Data;
+using FitnessTracker.Enums;
+using FitnessTracker.Models;
+using FitnessTracker.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTrackerTests.Services
+{
+    public class WorkoutServiceTests
+    {
+        private FitnessTrackerContext _context;
+        private WorkoutService _workoutService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<FitnessTrackerContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new FitnessTrackerContext(options);
+            _workoutService = new WorkoutService(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task AddWorkoutAsync_ShouldAddWorkout()
+        {
+            // Arrange
+            var workout = new Workout
+            {
+                Title = WorkoutTitle.Peito
+            };
+
+            // Act
+            await _workoutService.AddWorkoutAsync(workout);
+
+            // Assert
+            var workoutsInDb = await _context.Workouts.ToListAsync();
+
+            Assert.That(workoutsInDb.Count, Is.EqualTo(1));
+            Assert.That(workoutsInDb[0].Title, Is.EqualTo(WorkoutTitle.Peito));
+        }
+    }
+}
